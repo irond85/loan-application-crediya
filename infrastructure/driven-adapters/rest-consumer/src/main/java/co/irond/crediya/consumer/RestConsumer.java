7eca8fae01b7c0621c327dddcb3ebe173a0339ee
irond85 <ofocacion.34@gmail.com>
2025-08-28 22:53:27 -0500

    feat: implements path from application.yaml

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/irond/crediya/consumer/RestConsumer.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/irond/crediya/consumer/RestConsumer.java
@@ -5,6 +5,7 @@ import co.irond.crediya.model.exceptions.ErrorCode;
 import co.irond.crediya.model.user.UserGateway;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -16,12 +17,15 @@ import reactor.core.publisher.Mono;
 public class RestConsumer implements UserGateway {
     private final WebClient client;
 
+    @Value("${adapter.restconsumer.userEmailByDni}")
+    private String pathGetUserEmailByDni;
+
     @Override
     @CircuitBreaker(name = "getUserEmailByDni")
     public Mono<String> getUserEmailByDni(String dni) {
         return client
                 .get()
-                .uri("/dni/{dni}", dni)
+                .uri(pathGetUserEmailByDni, dni)
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
                     if (clientResponse.statusCode() == HttpStatus.NOT_FOUND) {
@@ -37,6 +41,5 @@ public class RestConsumer implements UserGateway {
                     }
                     return Mono.just("");
                 });
-
     }
 }
