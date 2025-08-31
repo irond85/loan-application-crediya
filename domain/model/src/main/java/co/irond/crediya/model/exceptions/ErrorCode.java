25be6252f4c3a6c4c8eb165690ab79c65dbfced1
irond85 <ofocacion.34@gmail.com>
2025-08-31 13:15:11 -0500

    feat: validation email user with email token

--- domain/model/src/main/java/co/irond/crediya/model/exceptions/ErrorCode.java
+++ domain/model/src/main/java/co/irond/crediya/model/exceptions/ErrorCode.java
@@ -5,9 +5,10 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     INVALID_LOAN_TYPE("BS_NE_001", "Not exists a loan type with this id.", 409),
-    USER_NOT_FOUND("BS_NE_002", "The user with dni doesn't exists.", 404),
+    USER_NOT_FOUND("BS_U_404_01", "The user with dni doesn't exists.", 404),
     DATABASE_ERROR("T_S_001", "An error has occurred while communicating with the database.", 500),
-    INVALID_TOKEN("BS_T_403_1", "Invalid authentication in the request.", 403);
+    INVALID_TOKEN("BS_T_403_01", "Invalid authentication in the request.", 403),
+    USER_NOT_MATCH("BS_U_401_01", "Can't do a loan application for different user.", 401);
 
     private final String internCode;
     private final String message;
