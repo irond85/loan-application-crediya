7e7a6f0751059770335852ebeb87637df4214411
irond85 <ofocacion.34@gmail.com>
2025-08-31 12:48:22 -0500

    feat: implements jwt in rest consumer external and loan applications endpoints

--- domain/model/src/main/java/co/irond/crediya/model/exceptions/ErrorCode.java
+++ domain/model/src/main/java/co/irond/crediya/model/exceptions/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ErrorCode {
     INVALID_LOAN_TYPE("BS_NE_001", "Not exists a loan type with this id.", 409),
     USER_NOT_FOUND("BS_NE_002", "The user with dni doesn't exists.", 404),
-    DATABASE_ERROR("T_S_001", "An error has occurred while communicating with the database.", 500);
+    DATABASE_ERROR("T_S_001", "An error has occurred while communicating with the database.", 500),
+    INVALID_TOKEN("BS_T_403_1", "Invalid authentication in the request.", 403);
 
     private final String internCode;
     private final String message;
